86d06baac3a6c5f640c0dd7db8075006161d590d
Luskalves <lusca.alves32@gmail.com>
2022-08-10 18:54:59 -0300

    requisito 10 concluido

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,21 +1,25 @@
-import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
   protected winner = 1;
   protected loser = -1;
   constructor(
-    protected player1: Character,
-    protected player2: Character,
+    protected player1: Fighter,
+    protected player2: Fighter,
   ) {
     super(player1);
   }
 
   fight(): number {
-    console.log(this.player1.strength);
-    this.player1.receiveDamage(this.player2.strength);
-    this.player2.attack(this.player2);
-    return this.player1.lifePoints;
+    while (this.player1.lifePoints > -1 && this.player2.lifePoints > -1) {
+      this.player1.receiveDamage(this.player2.strength);
+      this.player2.receiveDamage(this.player1.strength);
+    }
+    if (this.player1.lifePoints > -1) {
+      return this.winner;
+    }
+    return this.loser;
   }
 }
 
